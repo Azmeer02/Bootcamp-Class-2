81aa840ea1f29123d33e0cc87cda878dc0ae5ca3
Syed Azmeer Haider <syedazmeerhaider@gmail.com>
2020-10-09 18:00:16 +0500

    React Basic

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Class1 from './React Bootcamp/Bootcamp#1';
+
+function App() {
+  return (
+    <div>
+      <Class1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/React Bootcamp/Bootcamp#1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Class1() {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+}
+
+export default Class1;
